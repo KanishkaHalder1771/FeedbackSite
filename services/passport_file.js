838f29b074cedd74f1f20273bb864c52acc0a43e
KanishkaHalder1771 <kanishkahalder@gmail.com>
2020-06-29 22:11:29 +0530

    enabled proxy acess

--- services/passport_file.js
+++ services/passport_file.js
@@ -24,7 +24,8 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback',
+            proxy: true
         },
         (acessToken, refreshToken, profile, done) => {
             User.findOne({googleID: profile.id})
